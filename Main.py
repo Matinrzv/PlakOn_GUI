17a96198029a24aa7f9eaacd963021ca04c9ca47
Matinrzv <razavimatin36@gmail.com>
2026-02-16 11:57:09 +0330

    lesson 2 exercise: add reset button and dynamic title updates

--- Main.py
+++ Main.py
@@ -19,18 +19,26 @@ class MainWindow(QMainWindow):
         self.title_label = QLabel("Lesson 2: Button Click Event")
         self.status_label = QLabel("Button has not been clicked yet.")
         self.click_button = QPushButton("Click Me")
+        self.click_button_reset = QPushButton("Reset!")
 
         self.click_button.clicked.connect(self.on_button_clicked)
+        self.click_button_reset.clicked.connect(self.on_button_clicked_to_reset)
 
         layout.addWidget(self.title_label)
         layout.addWidget(self.status_label)
         layout.addWidget(self.click_button)
-
+        layout.addWidget(self.click_button_reset)
     def on_button_clicked(self):
         self.click_count += 1
+        self.setWindowTitle(f"Clicks: {self.click_count}")
         self.status_label.setText(f"Clicked {self.click_count} time(s).")
+        if self.click_count == 10:
+            self.status_label.setText("Great! 10 clicks reached.")
 
-
+    def on_button_clicked_to_reset(self):
+        self.click_count = 0
+        self.setWindowTitle("Clicks: 0")
+        self.status_label.setText(f"Clicked {self.click_count} time(s).")
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
