d6d2211667be7e7afec5a793f3608ae5c16916e5
Matinrzv <razavimatin36@gmail.com>
2026-02-16 11:21:07 +0330

    lesson 1: create first PyQt6 window

--- Main.py
+++ Main.py
@@ -1,23 +1,9 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, 
-                          QLabel, QPushButton, QStackedWidget)
+from PyQt6.QtWidgets import QApplication,QWidget
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.GUIinit()
-    def GUIinit(self):
-        button = QPushButton('Click!',self)
-        button.clicked.connect(self.button_clicked)
-        layout = QVBoxLayout()
-        layout.addWidget(button)
-        self.setLayout(layout)
-        self.setWindowTitle('PlakOn')
-        self.setGeometry(300,300,300,300)
-    def button_clicked(self):
-        print("Clicked...")
-if __name__=='__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("first app with PyQt6")
+window.resize(500,500)
+window.show()
+sys.exit(app.exec())
